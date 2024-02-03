34e5c79c47bf49b3f6e1c773bc338b0953dedbff
Sebastien Cambier <scambier@student.42.fr>
2024-02-03 22:35:31 +0100

    Added pipe to 2nd function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 19:05:41 by scambier          #+#    #+#             */
-/*   Updated: 2024/02/03 20:09:01 by scambier         ###   ########.fr       */
+/*   Updated: 2024/02/03 22:31:32 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 #include "libft.h"
 #include "str_exec.h"
@@ -22,10 +23,34 @@ int	main(int argc, char **argv, char **envp)
 {
 	int	infile;
 	int	outfile;
+	int	pipe_[2]; // 0:read 1:write
+	int	pids[2];
 
 	infile = open("InFile", O_RDONLY);
-	outfile = open("OutFile", O_CREAT | O_WRONLY);
-	dup2(infile, 0);
-	dup2(outfile, 1);
-	str_exec(argv[1], envp);
+	outfile = open("OutFile", O_TRUNC | O_CREAT | O_RDWR, 644);
+	if (pipe(pipe_))
+		printf("pipe Error\n");
+	
+	pids[0] = fork();
+	if (pids[0] == 0)
+	{
+		dup2(infile, 0);
+		dup2(pipe_[1], 1);
+		close(pipe_[0]);
+		str_exec(argv[1], envp);
+	}
+	pids[1] = fork();
+	if (pids[1] == 0)
+	{
+		dup2(pipe_[0], 0);
+		dup2(outfile, 1);
+		close(pipe_[1]);
+		str_exec(argv[2], envp);
+	}
+	close(pipe_[0]);
+	close(pipe_[1]);
+	close(infile);
+	close(outfile);
+	waitpid(pids[0], 0, 0);
+	waitpid(pids[1], 0, 0);
 }
