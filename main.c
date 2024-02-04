26bd3d6ae0fd22bbadabbbb175d2a9e55257ea0e
Sebastien Cambier <scambier@student.42.fr>
2024-02-05 00:32:00 +0100

    Fixed some errors

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 19:05:41 by scambier          #+#    #+#             */
-/*   Updated: 2024/02/04 14:04:18 by scambier         ###   ########.fr       */
+/*   Updated: 2024/02/05 00:30:03 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,15 @@ int	main(int argc, char **argv, char **envp)
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
-	if (pids[0] == 0)
+	if (pids[0] != 0)
 	{
-		files[1] = open(argv[4], O_TRUNC | O_CREAT | O_RDWR, 777);
+		files[1] = open(argv[4], O_TRUNC | O_CREAT | O_WRONLY, 777);
 		dup2(pipe_[0], 0);
 		close(pipe_[1]);
 		dup2(files[1], 1);
 		ret = str_exec(argv[3], envp);
+		close(files[1]);
+		close(pipe_[0]);
 		exit(EXIT_SUCCESS);
 	}
 	else
@@ -55,6 +57,8 @@ int	main(int argc, char **argv, char **envp)
 		close(pipe_[0]);
 		dup2(files[0], 0);
 		ret = str_exec(argv[2], envp);
+		close(files[0]);
+		close(pipe_[1]);
 		wait(0);
 	}
 	return (0);
