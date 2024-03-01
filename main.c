38309b44a8da7414d0876f0606f8fa6ca20c76d1
Sebastien Cambier <scambier@student.42.fr>
2024-03-01 20:10:26 +0100

    Reworked whole project to make it wayyyy cleaner

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 19:05:41 by scambier          #+#    #+#             */
-/*   Updated: 2024/02/28 19:45:25 by scambier         ###   ########.fr       */
+/*   Updated: 2024/03/01 20:08:31 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,60 +19,61 @@
 #include "libft.h"
 #include "str_exec.h"
 
-int	main(int argc, char **argv, char **envp)
-{
-	int	files[2];
-	int	pipe_[2];
-	int	pids[2];
-	int	ret;
+typedef struct s_command {
+	char	*command;
+	int		fd_in;
+	int		fd_out;
+}	t_command;
 
-	if (argc != 5)
-		return (1);
-	if (pipe(pipe_) == -1)
+int exe(t_command *cmd, char **envp)
+{
+	int	out;
+
+	dup2(cmd->fd_in, 0);
+	dup2(cmd->fd_out, 1);
+	out = str_exec(cmd->command, envp);
+	close(cmd->fd_in);
+	close(cmd->fd_out);
+	return (out);
+}
+
+void	pipe_exe(t_command *cmd1, t_command *cmd2, char **envp)
+{
+	int pid;
+	int	fd_pipe[2];
+
+	if (pipe(fd_pipe) == -1)
+		perror("pipex: pipe");
+	pid = fork();
+	if (pid == -1)
+		perror("pipex: fork");
+	else if (pid == 0)
 	{
-		perror("pipe");
-		exit(EXIT_FAILURE);
-	}
-	pids[0] = fork();
-	if (pids[0] == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	if (pids[0] != 0)
-	{
-		files[1] = open(argv[4], O_TRUNC | O_CREAT | O_WRONLY, 0644);
-		if (files[1] < 0)
-		{
-			perror("pipex: output");
-			exit(EXIT_FAILURE);
-		}
-		dup2(pipe_[0], 0);
-		close(pipe_[1]);
-		dup2(files[1], 1);
-		ret = str_exec(argv[3], envp);
-		close(pipe_[0]);
-		close(files[1]);
-		if (ret)
-			exit(EXIT_FAILURE);
+		cmd1->fd_out = fd_pipe[1];
+		close(fd_pipe[0]);
+		exe(cmd1, envp);
 	}
 	else
 	{
-		files[0] = open(argv[1], O_RDONLY);
-		if (files[0] < 0)
-		{
-			perror("pipex: input");
-			exit(EXIT_FAILURE);
-		}
-		dup2(pipe_[1], 1);
-		close(pipe_[0]);
-		dup2(files[0], 0);
-		ret = str_exec(argv[2], envp);
-		close(pipe_[1]);
-		close(files[0]);
-		if (ret)
-			exit(EXIT_FAILURE);
+		cmd2->fd_in = fd_pipe[0];
+		close(fd_pipe[1]);
+		exe(cmd2, envp);
 	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_command cmd1;
+	t_command cmd2;
+
+	if (argc != 5)
+		return (1);
+
+	cmd1.fd_in = open(argv[1], O_RDONLY);
+	cmd1.command = argv[2];
+	cmd2.command = argv[3];
+	cmd2.fd_out = open(argv[4], O_TRUNC | O_CREAT | O_WRONLY, 0644);
+	pipe_exe(&cmd1, &cmd2, envp);
 	wait(0);
 	return (0);
 }
